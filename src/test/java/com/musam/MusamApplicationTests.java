92cadf7502448b1e9b85d44a495229a56f608eda
David <alphateam.sistemas@upeu.edu.pe>
2019-10-29 06:30:21 -0500

    test

--- src/test/java/com/musam/MusamApplicationTests.java
+++ src/test/java/com/musam/MusamApplicationTests.java
@@ -1,13 +1,26 @@
 package com.musam;
 
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.musam.entity.Usuario;
+import com.musam.repository.UsuarioRepository;
 
 @SpringBootTest
 class MusamApplicationTests {
+	@Autowired
+	private UsuarioRepository repository;
 
 	@Test
-	void contextLoads() {
+	void guadarUsuario() {
+		Usuario us = new Usuario();
+		us.setUser("jonas");
+		us.setClave("12345");
+		repository.save(us);
+
 	}
 
 }
